694fda6ee0bd622fc8c39f4914f09523b2e94dab
Nikolay Lavrenyuk <ekaterina.lizina87@gmail.com>
2021-01-15 11:18:48 -0500

    Add HomeworkMain16

---
+++ src/main/java/fff/HomeworkMain16.java
@@ -0,0 +1,16 @@
+package fff;
+
+import java.util.Scanner;
+
+public class HomeworkMain16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Введите число");
+		int consoleNumber = Integer.parseInt(input.nextLine());
+		System.out.println("Число: " + consoleNumber);
+		input.close();
+	}
+
+}
